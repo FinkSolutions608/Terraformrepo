52230eab8371d800a54e9c3b6ca515ca9227ffb0
niels <niels.fink@ingrammicro.com>
2024-09-02 12:54:22 +0000

    umstrukturiert und den user data code angepasst

---
+++ ec2.tf
@@ -0,0 +1,25 @@
+
+
+
+# Create an EC2 instance in the first subnet
+resource "aws_instance" "web-frontend" {
+  ami                         = "ami-0501f3dfc19235d5f" # Win16
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.subnet_1.id
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.allow_ssh_http.id]
+  user_data                   = <<-EOF
+  #!/bin/bash
+  sudo yum update -y
+  sudo yum install -y httpd
+  sudo systemctl start httpd
+  sudo systemctl enable httpd
+  sudo echo '<center><h1> Dies ist Niels Webserver! </h1></center>' > /var/www/html/index.html
+
+EOF
+
+  tags = {
+    Name = "web_server"
+  }
+}
+
